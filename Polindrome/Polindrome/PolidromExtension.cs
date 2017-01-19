d34ad3a3023674d0b07826a089657f404c2584a6
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-01-20 00:16:37 +0400

    Added project to repository.

---
+++ Polindrome/Polindrome/PolidromExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polindrome
+{
+    public static class PolidromExtension
+    {
+        public static bool IsPalindrome(this string value)
+        {
+            for (int i = 0; i < value.Length / 2 + 1; i++)
+            {
+                if (value[i] != value[value.Length - i - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+    }
+}
